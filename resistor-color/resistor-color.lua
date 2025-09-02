1850d5d2377ad2a5aab1823501c4da9cb8f9b81d
michal-hynek <hynek.michal@gmail.com>
2025-09-02 07:05:47 -0700

    Resistor Colour

---
+++ resistor-color/resistor-color.lua
@@ -0,0 +1,18 @@
+local color_values = {
+  black = 0,
+  brown = 1,
+  red = 2,
+  orange = 3,
+  yellow = 4,
+  green = 5,
+  blue = 6,
+  violet = 7,
+  grey = 8,
+  white = 9,
+}
+
+return {
+  color_code = function(color)
+    return color_values[color];
+  end
+}
